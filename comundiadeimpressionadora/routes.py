389b80debd379bab6c01fc0bfb239cc9adcc2526
Guilherme-Fernandes97 <guilherme_fernandes1997@hotmail.com>
2022-11-15 22:15:32 -0300

    atualizado routes

--- comundiadeimpressionadora/routes.py
+++ comundiadeimpressionadora/routes.py
@@ -2,7 +2,7 @@ from flask import render_template, redirect, url_for, flash, request
 from comundiadeimpressionadora import app, database, bcrypt
 from comundiadeimpressionadora.forms import Formlogin, FormCriarConta
 from comundiadeimpressionadora.models import Usuario
-from flask_login import login_user
+from flask_login import login_user, logout_user, current_user, login_required
 
 lista_usuarios = ['Lira', 'João', 'Alon', 'Alessandra', 'Amanda']
 
@@ -17,6 +17,7 @@ def contato():
 
 
 @app.route('/usuarios')
+@login_required
 def usuarios():
     return render_template('usuarios.html', lista_usuarios=lista_usuarios)
 
@@ -25,16 +26,18 @@ def usuarios():
 def login():
     form_login = Formlogin()
     form_criarconta = FormCriarConta()
-
     if form_login.validate_on_submit() and 'botao_submit_login' in request.form:
         usuario = Usuario.query.filter_by(email=form_login.email.data).first()
         if usuario and bcrypt.check_password_hash(usuario.senha, form_login.senha.data):
             login_user(usuario, remember=form_login.lembrar_dados.data)
             flash(f'Login feito com sucesso no email {form_login.email.data}', "alert-success")
-            return redirect(url_for('home'))
+            par_next = request.args.get('next')
+            if par_next:
+                return redirect(par_next)
+            else:
+                return redirect(url_for('home'))
         else:
             flash(f'Falha no login, e-mail ou senha incorretos', "alert-danger")
-
     if form_criarconta.validate_on_submit() and 'botao_submit_criarconta' in request.form:
         senha_cript = bcrypt.generate_password_hash(form_criarconta.senha.data)
         usuario = Usuario(username=form_criarconta.username.data, email=form_criarconta.email.data, senha=senha_cript)
@@ -43,3 +46,25 @@ def login():
         flash(f'Conta criada para o e-mail {form_criarconta.email.data}', "alert-success")
         return redirect(url_for('home'))
     return render_template('login.html', form_login=form_login, form_criarconta=form_criarconta)
+
+
+@app.route('/sair')
+@login_required
+def sair():
+    logout_user()
+    flash(f'Logou feito com sucesso', "alert-success")
+    return redirect(url_for('home'))
+
+
+@app.route('/perfil')
+@login_required
+def perfil():
+    return render_template('perfil.html')
+
+
+@app.route('/post/criar')
+@login_required
+def criar_post():
+    return render_template('criarpost.html')
+
+
